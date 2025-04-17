4a7f689778481bb8a75a5abff0aa9eda77e45f36
Mohammad Abdullah <abdullah@rioforge.com>
2025-04-17 23:07:25 +0530

    Db connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDB connected successfully');
+}).catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
 const app = express();
 // const port  = process.env.PORT || 3000;
 app.listen(3000, () => {
