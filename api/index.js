0e804f943492e227288f4bbae622d3bdc7edbf94
Mohammad Abdullah <abdullah@rioforge.com>
2025-04-18 12:45:34 +0530

    Created test api route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('MongoDB connected successfully');
 }).catch((err) => {
     console.error('MongoDB connection error:', err);
 });
+
 const app = express();
 // const port  = process.env.PORT || 3000;
+app.use('/api/user', userRouter)
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
