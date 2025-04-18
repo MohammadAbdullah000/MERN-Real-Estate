dbdea0079471f6056b6f219b1a0277e5d6e07512
Mohammad Abdullah <abdullah@rioforge.com>
2025-04-18 15:46:52 +0530

    created middleware for error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -8,7 +8,9 @@ const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({ 'Message': "User created succesfully" })
     } catch (error) {
-        res.status(500).json(error.message)
+        // res.status(500).json(error.message)
+        // in production we dont everytime do above line for error, make middleware for errro in index.js and if you want to show error like psswor lenfth is not strong means custom error then create utils folder
+        next(error)
     }
 }
-export { signup }
+// export { signup }
--- api/index.js
+++ api/index.js
@@ -16,6 +16,15 @@ app.use(express.json())
 // const port  = process.env.PORT || 3000;
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
